21ca9b1c4aadd2a7dbd5ef0065892779fb0582e4
Denis Hihich <xixi4.den@gmail.com>
2019-04-20 00:26:38 +0300

    validate input [part1]

--- task1/changeSupplier.js
+++ task1/changeSupplier.js
@@ -1,8 +1,22 @@
 const times = require('lodash/times')
 
+const MAX_ALLOWED_EURO = process.env.MAX_ALLOWED_EURO || 500 // 500 is max Euro banknote
+
 const coinDenominations = [100, 50, 20, 10, 5, 2, 1]
 
+const validate = (euro) => {
+    if (euro < 0) {
+        throw new Error('input argument cannot be a negative number')
+    }
+
+    if (euro >= MAX_ALLOWED_EURO) {
+        throw new Error(`input argument cannot be greater than ${MAX_ALLOWED_EURO} Euro`)
+    }
+}
+
 function getOptimalChangeFor(euro) {
+    validate(euro)
+
     const res = []
 
     let remainingCoins = euro * 100
